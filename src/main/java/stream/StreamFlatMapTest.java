7d49ee49a00b895f2fe1eccb5bbf24dfe00e82b3
zhaoyangchang <396572268@qq.com>
2022-01-08 22:26:36 +0800

    20220108_zhaoyangchang_streamFlatMap code init

--- src/main/java/stream/StreamFlatMapTest.java
+++ src/main/java/stream/StreamFlatMapTest.java
@@ -5,8 +5,22 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class StreamTest {
+/**
+ * @className: StreamFlatMapTest
+ * @description: flatMap test
+ * @author zhaoyangchang
+ * @date: 2022/1/8 10:14 下午
+ * @version: 1.0.0
+ */
+public class StreamFlatMapTest {
+
+    private static Stream<List<Integer>> intStream = Stream.of(
+            Arrays.asList(4, 5),
+            Arrays.asList(1, 3, 6),
+            Arrays.asList(2, 8)
+    );
 
     private static List<String> list1 = Arrays.asList("zhangsan", "lisi", "wangwu");
 
@@ -18,7 +32,11 @@ public class StreamTest {
 
     public static void main(String[] args) {
 
-        // System.out.println(list2);
+        // 将复合的集合扁平化
+        List<Integer> flatI = intStream
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        System.out.println(flatI);
 
         // 合并两次map
         List<String> list = list2
