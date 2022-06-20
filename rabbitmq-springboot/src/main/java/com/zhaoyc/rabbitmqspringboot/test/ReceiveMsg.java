459bc37c559341b9f122118980ffe86884896d0e
zhaoyangchang <zhaoyangchang@huobi.com>
2022-06-20 21:02:46 +0800

    20220620_zhaoyangchang_spring-boot-mq init

---
+++ rabbitmq-springboot/src/main/java/com/zhaoyc/rabbitmqspringboot/test/ReceiveMsg.java
@@ -0,0 +1,49 @@
+package com.zhaoyc.rabbitmqspringboot.test;
+
+import com.rabbitmq.client.*;
+import com.zhaoyc.rabbitmqspringboot.utils.ConnectionUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author zhaoyangchang
+ * @version 1.0.0
+ * @className ReceiveMs
+ * @description: rabbitmq 消费者 test
+ * @date 2022/6/18 20:35
+ */
+@Component
+@Slf4j
+public class ReceiveMsg {
+
+    public void receiveMsg() {
+
+        // 1. 获取连接
+        try {
+            Connection connection = ConnectionUtil.getConnection();
+            Channel channel = connection.createChannel();
+
+            Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    // body 就是从队列中获取的数据
+                    String msg = new String(body);
+                    log.info("receive: {}", msg);
+                }
+            };
+
+            // 参数1：队列名
+            // 参数2：是否应答（消息确认）
+            // 参数3：consumer
+            // 多个消费者指定同一个queue，工作模式
+            channel.basicConsume("", true, consumer);
+
+        } catch (Exception e) {
+            log.warn("error: {}", e);
+        }
+
+    }
+
+}
